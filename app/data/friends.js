c6c3f1e0db69784544cbc3602176d948286436e6
Christelle Mangama <mangama_christelle@yahoo.fr>
2019-07-19 12:18:41 -0600

    Did most of the assignment. Need to work on having the form cleared after hitting submit and add friend looking for a friend in the friends array

---
+++ app/data/friends.js
@@ -0,0 +1,216 @@
+//Array of all the friends object
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://wallperio.com/data/out/2/actor-image_593413573.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Julie",
+        "photo": "https://images.complex.com/complex/images/c_limit,dpr_auto,q_90,w_720/fl_lossy,pg_1/xur7akqtkdaenulusrqs/famous-actresses-start-horror-michelle-williams",
+        "scores": [
+            5,
+            3,
+            2,
+            4,
+            1,
+            1,
+            2,
+            1,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Paul",
+        "photo": "https://img.cinemablend.com/cb/d/4/4/2/9/5/d442957a24af9799dd57c5a360cb03c5481696d1c7a45110fd16d4f90fd90da8.jpg",
+        "scores": [
+            1,
+            1,
+            2,
+            4,
+            5,
+            3,
+            2,
+            3,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Zac",
+        "photo": "https://chainimage.com/images/actor-zac-efron-wallpapers-and-images-wallpapers-pictures-photos.jpg",
+        "scores": [
+            2,
+            2,
+            4,
+            4,
+            2,
+            1,
+            2,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Louise",
+        "photo": "http://bloggersarena.com/celebs/10-Gorgeous-Hollywood-Actresses-Who-Went-Commando-For-The-Mo/4.png",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Ben",
+        "photo": "https://i.pinimg.com/originals/d6/96/2d/d6962dcccdf52bd38d187f1c0e707a87.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            1,
+            5,
+            2,
+            5,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Tom",
+        "photo": "http://top10for.com/wp-content/uploads/2014/03/Tom-Cruise.jpg",
+        "scores": [
+            5,
+            1,
+            3,
+            4,
+            5,
+            3,
+            2,
+            3,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Hugh",
+        "photo": "https://tse4.mm.bing.net/th?id=OIP.LOyuwb8lPGk2tDRn_k4plQEsD6&pid=Api&P=0&w=217&h=182",
+        "scores": [
+            3,
+            1,
+            3,
+            3,
+            5,
+            3,
+            2,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Young",
+        "photo": "https://compote.slate.com/images/fd268400-c348-4021-82bb-3d0a1962e257.jpg",
+        "scores": [
+            1,
+            3,
+            4,
+            3,
+            5,
+            3,
+            2,
+            3,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Rowan",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/RowanAtkinsonMar07.jpg/220px-RowanAtkinsonMar07.jpg",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            1,
+            2,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Diane",
+        "photo": "https://cld.fame10.com/image/upload/t_tn,f_auto,q_auto,$h_400,$w_620/f10/2015/02/shutterstock_181485362-620x400.jpg",
+        "scores": [
+            5,
+            1,
+            3,
+            4,
+            2,
+            2,
+            2,
+            5,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Diana",
+        "photo": "https://i.pinimg.com/originals/17/ce/2e/17ce2e4390b3ef77041cf510e48246ae.jpg",
+        "scores": [
+            1,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            1,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Ellice",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfYPxQTY3y47nGIuj5GTaplBL5ubB4qQ6_d6n_h4HUwhAFYAlgKg",
+        "scores": [
+            2,
+            1,
+            1,
+            2,
+            5,
+            1,
+            2,
+            1,
+            1,
+            1
+        ]
+    },
+
+]
+
+
+//Export the data
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,73 @@
+//Getting data from the friends array
+var friends = require('../data/friends.js');
+
+//Allows the export of the app 
+module.exports = function(app) {
+
+    // GET-api/friends route
+    app.get('/api/friends', function(req, res) {
+        res.json(friends);
+    });
+
+    // POST-api/friends route 
+    app.post('/api/friends', function(req, res) {
+
+
+
+
+
+
+
+
+
+
+
+        
+    		// Set variables only needed for the post
+        var difference = 40;
+        var matchName = '';
+        var matchPhoto = '';
+
+        // Goes through the friends array in the fiends.js file
+        friends.forEach(function(friend) {
+        		// Variables for comparing matches
+            var matchedScoresArray = [];
+            var totalDifference = 40;
+
+            // Function to assist in the addition reduce() below
+            function add(total, num) {
+                return total + num;
+            }
+
+            // This loops through each item of the scores arrays
+            // from both the stored data and the new user, 
+            // and then substracts, absolutes, and then pushes the 
+            // new value to the matchedScoresArray
+            for (var i = 0; i < friend.scores.length; i++) {
+                matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friend.scores[i])));
+
+            }
+
+            // This reduces the matchScoresArray into a single value in a variable
+            totalDifference = matchedScoresArray.reduce(add, 0);
+
+            // If the above value is smaller than the previous difference...
+            if (totalDifference < difference) {
+            		// Set it as the previous difference...
+                difference = totalDifference;
+                // And set these variables to the appropriate friend match
+                matchName = friend.name;
+                matchPhoto = friend.photo;
+            }
+        });
+        // Once the cycle is complete, the match with the least difference will remain,
+        // and that data will be sent as a json object back to the client
+        res.json({
+            name: matchName,
+            photo: matchPhoto
+        });
+
+        // This adds the new users sent data object to friends.js
+        friends.push(req.body);
+    });
+}
